9b85fda2828298ef80d82ab8d007d2a20be82fe3
sebha01 <30077548@students.southwales.ac.uk>
2025-04-08 13:20:29 +0100

    Revert "More progress"
    This reverts commit 41cd3aaba871cb29380f1674bece4651559d63c7.

--- Server/GameServer.h
+++ Server/GameServer.h
@@ -5,14 +5,14 @@
 class GameServer
 {
 	private:
-		int playerOneChoice[2]; // Player 1's two choices
-		int playerTwoChoice[2]; // Player 2's two choices
-		bool gameOver;          // To track if the game is over
+		char grid[16] = { 'a', 'a', 'b', 'b', 'c', 'c', 'd', 'd', 'e', 'e', 'f', 'f', 'g', 'g', 'h', 'h' };  //this should be unaccessable to the client, only the server knows the board state
 	public:
 
 		GameServer() 
 		{
-			gameOver = false;
+			//randomise Grid!
+			random_shuffle(&grid[0], &grid[16]);
+
 		}
 
 		int RecieveInputOne(int card1, int card2) 
@@ -29,6 +29,28 @@ class GameServer
 		{
 			int counter = 0;						//This resets the counter
 			int returnflag = 0;
+			if ((grid[card1] == 'x') || (grid[card2] == 'x'))		//This 'if statment' states if you have made the choice 
+			{
+				returnflag = 1;//cout << ("\nUnfortunately you have selected a card already matched, please try again\n\n");
+
+				//try again
+			}
+			else if ((grid[card1] == grid[card2]) && (grid[card1] != 'x') && (grid[card2] != 'x')) //This is the 'if statment' that will match the pairs and then mark them as 'x' 
+			{
+				//cout << ("\nCongratulations you have found a pair!!\n");
+				grid[card1] = 'x';
+				grid[card2] = 'x';
+				returnflag = 2;
+				//pair
+			}
+			else if (((grid[card1] != grid[card2]) && (grid[card1] != 'x') && (grid[card2] != 'x'))) //This 'if statment' states if the guesses are incorrect
+			{
+				returnflag = 3;//cout << ("\nOopps remember the cards as you need to try again\n\n");
+				//fail
+			}
+			if (checkwin()) {returnflag = 4;}
+
+			return returnflag;
 		}
 
 		bool checkwin() 
@@ -37,12 +59,46 @@ class GameServer
 			bool found = false;
 			int i = 0;
 
+			while ((found == false) and (i < 16)) 
+			{		//Checks through the board for anything that isnt an x(to signify its been found)
+				if (grid[i] != 'x') 
+				{
+					found = true;
+				}
+
+				i++;
+			}
 			return not found;
 		}
 
 		void SendMap(int card1, int card2, char OutGrid[16], bool currentPlayer)	 
 		{
-  
+			//char returngrid[17];
+
+			for (int i = 0; i < 16; i++) 
+			{
+				if (grid[i] == 'x') 
+				{
+					OutGrid[i] = 'x';
+				}
+				else if ((i == card1) or (i == card2)) 
+				{
+					if (currentPlayer) 
+					{
+						OutGrid[i] = grid[i];
+					}
+					else 
+					{
+						OutGrid[i] = '?';
+					}
+				}
+				else 
+				{
+					OutGrid[i] = ' ';
+				}
+			}
+			//Send the empty map, with cleared boxes shown
+			//OutGrid = returngrid;
 		}
 };
 
